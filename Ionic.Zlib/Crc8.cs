516e14ca8e4759cd01ed969d0d4d15e02a5e7f48
Bohdan Reminnyi <breminnyi@playtika.com>
2020-04-19 20:31:15 +0300

    Crc8 implemented w/ lookup table and covered w/ tests

--- Ionic.Zlib/Crc8.cs
+++ Ionic.Zlib/Crc8.cs
@@ -5,27 +5,46 @@ namespace Ionic.Zlib
 {
     public class Crc8
     {
-        public static byte CalculateSimple(byte[] input, ushort generator)
+        public static Crc8 Default { get; } = new Crc8(7);
+
+        private readonly byte[] _lookupTable;
+
+        public Crc8(byte generator)
         {
-            ushort current = 0;
-            input = input.Append((byte) 0).ToArray();
-            for (var index = 0; index < input.Length; index++)
+            _lookupTable = new byte[256];
+            for (var @byte = 0; @byte < 256; @byte++)
             {
-                for (var offset = 7; offset >=0; offset--)
+                var crc = (byte) @byte;
+                for (byte bit = 0; bit < 8; bit++)
                 {
-                    if ((current & 0b1000_0000) == 0b1000_0000) // if MSB == 1
+                    var msbSet = (crc & 0b1000_0000) == 0b1000_0000;
+                    crc <<= 1;
+                    if (msbSet)
                     {
-                        current = (ushort) ((current << 1) + ((input[index] >> offset) & 0x1));
-                        current = (ushort) (current ^ generator);
-                    }
-                    else
-                    {
-                        current = (ushort) ((current << 1) + ((input[index] >> offset) & 0x1));
+                        crc ^= generator;
                     }
                 }
+
+                _lookupTable[@byte] = crc;
+            }
+        }
+
+        public byte Compute(byte[] input)
+        {
+            return Compute(input, 0, input.Length);
+        }
+
+        public byte Compute(byte[] input, int offset, int count)
+        {
+            byte crc = 0;
+            // ReSharper disable once ForCanBeConvertedToForeach
+            for (var index = offset; index < offset + count; index++)
+            {
+                var data = input[index] ^ crc;
+                crc = _lookupTable[data];
             }
 
-            return (byte) current;
+            return crc;
         }
     }
 }
--- Tests/Ionic.Zlib.Tests/Crc8Tests.cs
+++ Tests/Ionic.Zlib.Tests/Crc8Tests.cs
@@ -6,12 +6,13 @@ namespace Ionic.Zlib
     public class Crc8Tests
     {
         [Theory]
-        [InlineData(new byte[] {0xC2}, 0x11D, 0xF)]
+        [InlineData(new byte[] {0xC2}, 0x1D, 0xF)]
         [InlineData(new byte[] {0xAB, 0xCD, 0xEF}, 0x7, 0x23)]
         [InlineData(new byte[] {0x01, 0x23, 0x45, 0x67,0x89}, 0xD5, 0x4C)]
-        public void CalculateSimpleShouldReturnCorrectResult(byte[] input, ushort generator, byte expected)
+        public void ComputeShouldReturnCorrectResult(byte[] input, byte generator, byte expected)
         {
-            var actual = Crc8.CalculateSimple(input, generator);
+            var crc8 = new Crc8(generator);
+            var actual = crc8.Compute(input);
             Assert.Equal(expected, actual);
         }
     }
