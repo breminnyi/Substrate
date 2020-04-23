52a9999c8fefea21c225c7a5a124e01f2bee5af4
Bohdan Reminnyi <breminnyi@playtika.com>
2020-04-23 13:09:16 +0300

    added two more helper methods to reverse bits in UInt16 and UInt32 values

--- Ionic.Zlib/Utilities/BitHelper.cs
+++ Ionic.Zlib/Utilities/BitHelper.cs
@@ -13,7 +13,32 @@ namespace Ionic.Zlib.Utilities
                 // 0x02211 = 0b0000_‭0010_0010_0001_0001‬
                 // 0x88440 = 0b‭1000_1000_0100_0100_0000‬
                 // 0x10101 = 0b‭0001_0000_0001_0000_0001‬
-                return (byte)(((value * 0x0802u & 0x22110u) | (value * 0x8020u & 0x88440u)) * 0x10101u >> 16);
+                return (byte) (((value * 0x0802u & 0x22110u) | (value * 0x8020u & 0x88440u)) * 0x10101u >> 16);
+            }
+        }
+
+        public static ushort Reverse(ushort value)
+        {
+            unchecked
+            {
+                value = (ushort) ((value >> 1) & 0x5555 | (value << 1) & 0xAAAA);
+                value = (ushort) ((value >> 2) & 0x3333 | (value << 2) & 0xCCCC);
+                value = (ushort) ((value >> 4) & 0x0F0F | (value << 4) & 0xF0F0);
+                value = (ushort) ((value >> 8) | (value << 8));
+                return value;
+            }
+        }
+
+        public static uint Reverse(uint value)
+        {
+            unchecked
+            {
+                value = (value >> 01) & 0x5555_5555 | (value << 01) & 0xAAAA_AAAA;
+                value = (value >> 02) & 0x3333_3333 | (value << 02) & 0xCCCC_CCCC;
+                value = (value >> 04) & 0x0F0F_0F0F | (value << 04) & 0xF0F0_F0F0;
+                value = (value >> 08) & 0x00FF_00FF | (value << 08) & 0xFF00_FF00;
+                value = (value >> 16) | (value << 16);
+                return value;
             }
         }
     }
--- Tests/Ionic.Zlib.Tests/Utilities/BitHelperTests.cs
+++ Tests/Ionic.Zlib.Tests/Utilities/BitHelperTests.cs
@@ -16,5 +16,31 @@ namespace Ionic.Zlib.Utilities
             var actual = BitHelper.Reverse(input);
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData(0b0000_0000_1111_1111, 0b1111_1111_0000_0000)]
+        [InlineData(0b1010_1010_1010_1010, 0b0101_0101_0101_0101)]
+        [InlineData(0b1100_1100_1100_1100, 0b0011_0011_0011_0011)]
+        [InlineData(0b1100_1100_0011_0011, 0b1100_1100_0011_0011)]
+        [InlineData(0x0000, 0x0000)]
+        [InlineData(0xFFFF, 0xFFFF)]
+        public void ReverseShouldHandleUInt16Input(ushort input, ushort expected)
+        {
+            var actual = BitHelper.Reverse(input);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(0x0000_0000, 0x0000_0000)]
+        [InlineData(0xFFFF_FFFF, 0xFFFF_FFFF)]
+        [InlineData(0x0000_FFFF, 0xFFFF_0000)]
+        [InlineData(0x0000_00F0, 0x0F00_0000)]
+        [InlineData(0b0111, 0b1110_0000_0000_0000_0000_0000_0000_0000)]
+        [InlineData(0b1100_0100, 0b0010_0011_0000_0000_0000_0000_0000_0000)]
+        public void ReverseShouldHandleUInt32Input(uint input, uint expected)
+        {
+            var actual = BitHelper.Reverse(input);
+            Assert.Equal(expected, actual);
+        }
     }
 }
